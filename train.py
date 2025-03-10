ed9e861584453b13865cf16603b5946489b8d6cd
Akshath <akshathmangudi@gmail.com>
2025-03-10 12:35:18 +0530

    switching to cifar100

--- train.py
+++ train.py
@@ -1,6 +1,5 @@
 import os
 import torch
-import datetime
 import argparse
 import logging
 from torch.optim import Adam
@@ -8,37 +7,31 @@ from tqdm import tqdm, trange
 from torchvision import transforms
 from model import VisionTransformer
 from torch.utils.data import DataLoader
-from torchvision.datasets import MNIST, ImageNet
+from torchvision.datasets import CIFAR100
 from utils import calculate_metrics, save_metrics, setup_logging
-
 
 def main(train_loader, test_loader, args):
     device = torch.device(args.device)
     logging.info(f"Using device: {device} " +
                  (f"({torch.cuda.get_device_name(device)})" if torch.cuda.is_available() else ""))
 
-    # mnist_model = VisionTransformer(
-    #     (1, 28, 28), n_patches=7, n_blocks=2, d_hidden=8, n_heads=2, out_d=10,
-    #     type=args.model_type).to(device)
-
-    mnist_model = VisionTransformer(
-        (3, 224, 224), n_patches=14, n_blocks=args.n_blocks, d_hidden=args.d_hidden, n_heads=args.n_heads, out_d=1000,
+    model = VisionTransformer(
+        (3, 32, 32), n_patches=4, n_blocks=args.n_blocks, d_hidden=args.d_hidden, n_heads=args.n_heads, out_d=100,
         type=args.model_type).to(device)
 
     criterion = torch.nn.CrossEntropyLoss()
-    optimizer = Adam(mnist_model.parameters(), lr=args.learning_rate)
-
+    optimizer = Adam(model.parameters(), lr=args.learning_rate)
     metrics_log_filename = setup_logging(args.log_dir)
 
     for epoch in trange(args.epochs, desc="train"):
         train_loss = 0.0
         y_true_train, y_pred_train, y_pred_proba_train = [], [], []
 
-        mnist_model.train()
+        model.train()
         for batch in tqdm(train_loader, desc=f"Epoch {epoch + 1} in training", leave=False):
             x, y = batch
             x, y = x.to(device), y.to(device)
-            y_hat = mnist_model(x)
+            y_hat = model(x)
             loss = criterion(y_hat, y)
 
             train_loss += loss.detach().cpu().item() / len(train_loader)
@@ -50,8 +43,7 @@ def main(train_loader, test_loader, args):
             y_pred_train.extend(torch.argmax(y_hat, dim=1).cpu().numpy())
             y_pred_proba_train.extend(torch.nn.functional.softmax(y_hat, dim=1).detach().cpu().numpy())
 
-        accuracy, balanced_accuracy, f1, roc_auc = calculate_metrics(
-            y_true_train, y_pred_train, y_pred_proba_train)
+        accuracy, balanced_accuracy, f1, roc_auc = calculate_metrics(y_true_train, y_pred_train, y_pred_proba_train)
 
         logging.info(f"Epoch {epoch + 1}/{args.epochs}")
         logging.info(f"  Train Loss: {train_loss:.4f}")
@@ -61,11 +53,10 @@ def main(train_loader, test_loader, args):
         logging.info(f"  Train ROC AUC: {roc_auc:.4f}")
 
         if epoch == args.epochs - 1:
-            save_metrics(metrics_log_filename, epoch + 1, "Train",
-                         train_loss, accuracy, balanced_accuracy, f1, roc_auc, flag=0)
+            save_metrics(metrics_log_filename, epoch + 1, "Train", train_loss, accuracy, balanced_accuracy, f1, roc_auc, flag=0)
 
     # Testing
-    mnist_model.eval()
+    model.eval()
     with torch.no_grad():
         test_loss = 0.0
         y_true_test, y_pred_test, y_pred_proba_test = [], [], []
@@ -73,7 +64,7 @@ def main(train_loader, test_loader, args):
         for batch in tqdm(test_loader, desc="Testing"):
             x, y = batch
             x, y = x.to(device), y.to(device)
-            y_hat = mnist_model(x)
+            y_hat = model(x)
             loss = criterion(y_hat, y)
             test_loss += loss.detach().cpu().item() / len(test_loader)
 
@@ -81,8 +72,7 @@ def main(train_loader, test_loader, args):
             y_pred_test.extend(torch.argmax(y_hat, dim=1).cpu().numpy())
             y_pred_proba_test.extend(torch.nn.functional.softmax(y_hat, dim=1).cpu().numpy())
 
-        accuracy, balanced_accuracy, f1, roc_auc = calculate_metrics(
-            y_true_test, y_pred_test, y_pred_proba_test)
+        accuracy, balanced_accuracy, f1, roc_auc = calculate_metrics(y_true_test, y_pred_test, y_pred_proba_test)
 
         logging.info("Test Results:")
         logging.info(f"  Test Loss: {test_loss:.4f}")
@@ -91,66 +81,39 @@ def main(train_loader, test_loader, args):
         logging.info(f"  Test F1 Score: {f1:.4f}")
         logging.info(f"  Test ROC AUC: {roc_auc:.4f}")
 
-        save_metrics(metrics_log_filename, args.epochs, "Test", test_loss,
-                     accuracy, balanced_accuracy, f1, roc_auc, flag=1)
-
+        save_metrics(metrics_log_filename, args.epochs, "Test", test_loss, accuracy, balanced_accuracy, f1, roc_auc, flag=1)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Benchmark KAN-based Vision Transformers')
-    parser.add_argument('--epochs', type=int, default=20,  # increased from 5 to 20
-                        help='number of epochs to train')
-    parser.add_argument('--batch-size', type=int, default=128,
-                        help='batch size for training')
-    parser.add_argument('--learning-rate', type=float, default=0.001,
-                        help='learning rate for optimizer')
-    parser.add_argument('--device', type=str,
-                        default='cuda' if torch.cuda.is_available() else 'cpu',
-                        help='device to use for training (cuda/cpu)')
-    parser.add_argument('--model-type', type=str, default='vanilla',
-                        help='variant to run, e.g.: [vanilla, efficientkan, sine, fourier, fast]')
-    parser.add_argument('--n-blocks', type=int, default=4,   # bigger than 2
-                        help='number of transformer blocks')
-    parser.add_argument('--d-hidden', type=int, default=64,  # bigger than 8
-                        help='hidden dimension of each transformer block')
-    parser.add_argument('--n-heads', type=int, default=2,
-                        help='number of attention heads')
-    parser.add_argument('--log-dir', type=str, default='logs',
-                        help='directory to store logs')
+    parser = argparse.ArgumentParser(description='Benchmark Vision Transformer on CIFAR-100')
+    parser.add_argument('--epochs', type=int, default=20, help='number of epochs to train')
+    parser.add_argument('--batch-size', type=int, default=128, help='batch size for training')
+    parser.add_argument('--learning-rate', type=float, default=0.001, help='learning rate for optimizer')
+    parser.add_argument('--device', type=str, default='cuda' if torch.cuda.is_available() else 'cpu', help='device to use for training (cuda/cpu)')
+    parser.add_argument('--model-type', type=str, default='vanilla', help='variant to run')
+    parser.add_argument('--n-blocks', type=int, default=4, help='number of transformer blocks')
+    parser.add_argument('--d-hidden', type=int, default=64, help='hidden dimension of transformer block')
+    parser.add_argument('--n-heads', type=int, default=2, help='number of attention heads')
+    parser.add_argument('--log-dir', type=str, default='logs', help='directory to store logs')
     args = parser.parse_args()
 
-    # transform = transforms.ToTensor()
-    # train_mnist = MNIST(root='./mnist', train=True,
-    #                     download=True, transform=transform)
-    # test_mnist = MNIST(root='./mnist', train=False,
-    #                    download=True, transform=transform)
-
-    # train_loader = DataLoader(
-    #     train_mnist, shuffle=True, batch_size=args.batch_size)
-    # test_loader = DataLoader(test_mnist, shuffle=False,
-    #                          batch_size=args.batch_size)
-    
-    imagenet_train_transforms = transforms.Compose([
-    transforms.RandomResizedCrop(224),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    ])
-
-    imagenet_val_transforms = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
+    # CIFAR-100 Transformations
+    cifar_train_transforms = transforms.Compose([
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomCrop(32, padding=4),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        transforms.Normalize(mean=[0.5071, 0.4867, 0.4408], std=[0.2675, 0.2565, 0.2761])
     ])
 
-    # Load datasets
-    imagenet_train = ImageNet(root='/kaggle/working/train', transform=imagenet_train_transforms)
-    imagenet_val = ImageNet(root='/kaggle/working/test', transform=imagenet_val_transforms)
+    cifar_test_transforms = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5071, 0.4867, 0.4408], std=[0.2675, 0.2565, 0.2761])
+    ])
 
-    # Create DataLoaders
-    train_loader = DataLoader(imagenet_train, batch_size=args.batch_size, shuffle=True, num_workers=8, pin_memory=True)
-    val_loader = DataLoader(imagenet_val, batch_size=args.batch_size, shuffle=False, num_workers=8, pin_memory=True)
+    # Load CIFAR-100 Dataset
+    train_dataset = CIFAR100(root='./cifar100', train=True, download=True, transform=cifar_train_transforms)
+    test_dataset = CIFAR100(root='./cifar100', train=False, download=True, transform=cifar_test_transforms)
 
-    main(train_loader=train_loader,
-         test_loader=val_loader,
-         args=args)
+    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=8, pin_memory=True)
+    test_loader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False, num_workers=8, pin_memory=True)
+
+    main(train_loader=train_loader, test_loader=test_loader, args=args)
